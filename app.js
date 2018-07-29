793d930d107a93fab67241319953e1431273875a
Kevin Herklotz <scheek.hee@gmail.com>
2018-07-29 13:56:08 +0200

    added html and style

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const path = require('path');
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+app.use(express.static(__dirname));
 
 app.listen(3000, () => console.log('...you can open http://localhost:3000/ now.'))
